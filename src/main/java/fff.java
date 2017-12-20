7b7dbb7ffde7211f3453b08c6a224fd1a8c09633
Lenovo <Lenovo@DESKTOP-FPL8T9S>
2017-12-20 19:32:55 +0800

    drgg

---
+++ src/main/java/fff.java
@@ -0,0 +1,25 @@
+/**
+ * Project Name:dt59-liupeng
+ * File Name:fff.java
+ * Package Name:
+ * Date:2017年12月20日下午7:31:32
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+/**
+ * Description:   <br/>
+ * Date:     2017年12月20日 下午7:31:32 <br/>
+ * @author   LiuPeng
+ * @version
+ * @see
+ */
+public class fff {
+
+    public static void main(String[] args) {
+
+        //  Auto-generated method stub
+
+    }
+
+}
+
